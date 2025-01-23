1badead694f54635b5fb5df0e51d9e100d9e90c3
Artiom <moiseevartem-7@ya.ru>
2025-01-23 23:29:05 +0400

    feat(init): setup empty project with backend

---
+++ src/main/java/org/alpharybackend/AlpharyBackendApplication.java
@@ -0,0 +1,11 @@
+package org.alpharybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlpharyBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AlpharyBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/org/alpharybackend/AlpharyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alpharybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlpharyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
